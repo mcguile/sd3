5cec78f78a3de3ab6fb7d808b9d337aaa9091eb8
Connor <cmcguile@gmail.com>
2016-02-14 00:57:42 +0000

    Reverse Stack implemented

--- Lab5/src/Q1.java
+++ Lab5/src/Q1.java
@@ -5,7 +5,11 @@ public class Q1 {
 	 * 1: complete implementation
 	 */
 	public static void reverseStack(Stack st){
-
+		Stack temp = new Stack();
+		for (int i=0; i<st.size(); i++) {
+			temp.push(st.pop());
+		}
+		st = temp;
 	}
 	
 	
