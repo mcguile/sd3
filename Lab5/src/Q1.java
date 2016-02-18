c15367ca5bb81433b4183be8da84a36d4aff4408
Connor <cmcguile@gmail.com>
2016-02-18 13:51:51 +0000

    Lab 5 complete

--- Lab5/src/Q1.java
+++ Lab5/src/Q1.java
@@ -5,11 +5,13 @@ public class Q1 {
 	 * 1: complete implementation
 	 */
 	public static void reverseStack(Stack st){
-		Stack temp = new Stack();
-		for (int i=0; i<st.size(); i++) {
-			temp.push(st.pop());
+		Queue queue = new Queue();
+		while (!st.isEmpty()) {
+			queue.enqueue(st.pop());
 		}
-		st = temp;
+		while (!queue.isEmpty()) {
+			st.push(queue.dequeue());
+		}
 	}
 	
 	
