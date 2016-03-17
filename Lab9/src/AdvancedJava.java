08ccc71ef47b648e05d05ddcf8aac42b0b4d979c
Connor <cmcguile@gmail.com>
2016-03-17 14:13:18 +0000

    Lab 9 complete

--- Lab9/src/AdvancedJava.java
+++ Lab9/src/AdvancedJava.java
@@ -1,4 +1,5 @@
 import java.util.Comparator;
+import java.util.function.BiPredicate;
 
 public class AdvancedJava {
 
@@ -10,11 +11,24 @@ public class AdvancedJava {
 		String[] strs = {"D","E","X","A"};
 		Integer[] ints = {4,5,6,1,12,23};
 		
-		
 		/*
 		 * Part 5: sort both strs and ints using gs and gi respectively
 		 *   You should use an anonymous instantce of the Comparator as illustrated in the lecture
 		 */
+		
+		gs.insertionSort(strs, new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				return s1.compareTo(s2);
+			}
+		});
+		
+		gi.insertionSort(ints, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1.compareTo(o2);
+			}
+		});
 		
 		printIntArray(ints);
 		printStringArray(strs);
@@ -30,6 +44,12 @@ public class AdvancedJava {
 		 *   You should use lambda expressions as illustrated in the lecture
 		 */		
 
+		BiPredicate<String,String> biStr = (t,u) -> t.compareTo(u)<0;
+		gs.insertionSort(strs, biStr);
+		
+		BiPredicate<Integer,Integer> biInt = (t,u) -> t<u;
+		gi.insertionSort(ints, biInt);
+		
 		printIntArray(ints);
 		printStringArray(strs);		
 	}
--- Lab9/src/GenericSort.java
+++ Lab9/src/GenericSort.java
@@ -6,11 +6,26 @@ public class GenericSort<E> {
 	
 	// Part 5
 	public void insertionSort(E[] list,Comparator<E> comp){
+		for (int i=0; i < list.length; i++) {
+		    /* Insert a[i] into the sorted sublist */
+		    E v = list[i];
+		    int j;
+		    for (j = i - 1; j >= 0; j--) {
+		        if (comp.compare(list[j], v) <= 0) break;
+		        list[j + 1] = list[j];
+		    }
+		    list[j + 1] = v; }}
 
-	}
-	
 	// Part 6
 	public void insertionSort(E[] list,BiPredicate<E,E> pred){
-		
-	}
+		for (int i=0; i < list.length; i++) {
+		    /* Insert a[i] into the sorted sublist */
+		    E v = list[i];
+		    int j;
+		    for (j = i - 1; j >= 0; j--) {
+		        if (pred.test(list[j], v)) break;
+		        list[j + 1] = list[j];
+		    }
+		    list[j + 1] = v; }}
+	
 }
